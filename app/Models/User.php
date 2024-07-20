a81a836951ac9be4f6a1db328097ae30616fd71a
SHIVAM SINGH <157016461+Shivamds15@users.noreply.github.com>
2024-07-20 20:00:21 +0530

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,44 +10,23 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password', 'profile_picture',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    /**
-     * Get the posts for the user.
-     */
     public function posts()
     {
         return $this->hasMany(Post::class);
     }
 
-    /**
-     * Get the comments for the user.
-     */
     public function comments()
     {
         return $this->hasMany(Comment::class);
